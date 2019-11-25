fe68999490cf2b2d2eec86f30349f4996ff9f791
Eleonore Hebrard <hebrard.eleonore@gmail.com>
2019-11-25 16:13:20 +0000

    movies and users seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Movie.destroy_all
+User.destroy_all
+
+user1 = User.create(email: "test123@test.com", password: "123456")
+user2 = User.create(email: "test1234@test.com", password: "123456")
+user3 = User.create(email: "test12345@test.com", password: "123456")
+
+
+movie1 = Movie.create(title: 'Pulp Fiction', picture_url: 'https://www.imdb.com/title/tt0110912/mediaviewer/rm1959546112')
+movie2 = Movie.create(title: 'Fight Club', picture_url: 'https://m.media-amazon.com/images/M/MV5BMmEzNTkxYjQtZTc0MC00YTVjLTg5ZTEtZWMwOWVlYzY0NWIwXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SY1000_CR0,0,666,1000_AL_.jpg')
+movie3 = Movie.create(title: 'Interstellar', picture_url: 'https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SY1000_SX675_AL_.jpg')
+movie4 = Movie.create(title: 'Gladiator', picture_url: 'https://m.media-amazon.com/images/M/MV5BMDliMmNhNDEtODUyOS00MjNlLTgxODEtN2U3NzIxMGVkZTA1L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SY1000_CR0,0,675,1000_AL_.jpg')
+movie5 = Movie.create(title: 'Momento', picture_url: 'https://m.media-amazon.com/images/M/MV5BZTcyNjk1MjgtOWI3Mi00YzQwLWI5MTktMzY4ZmI2NDAyNzYzXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SY1000_CR0,0,681,1000_AL_.jpg')
+movie6 = Movie.create(title: 'Joker', picture_url: 'https://m.media-amazon.com/images/M/MV5BNGVjNWI4ZGUtNzE0MS00YTJmLWE0ZDctN2ZiYTk2YmI3NTYyXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SY1000_CR0,0,674,1000_AL_.jpg')

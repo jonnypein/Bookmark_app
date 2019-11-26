115a651959f5735d6d7b86f890acee1fdf91dfd8
Eleonore Hebrard <hebrard.eleonore@gmail.com>
2019-11-26 13:26:41 +0000

    updates

--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_25_150520) do
-
+ActiveRecord::Schema.define(version: 2019_11_26_104043) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
   create_table "friends", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture_url"
+    t.string "description"
+    t.string "genre"
+    t.integer "year"
   end
-
   create_table "recommendations", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "movie_id"
@@ -34,7 +34,6 @@ ActiveRecord::Schema.define(version: 2019_11_25_150520) do
     t.index ["movie_id"], name: "index_recommendations_on_movie_id"
     t.index ["user_id"], name: "index_recommendations_on_user_id"
   end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -46,7 +45,6 @@ ActiveRecord::Schema.define(version: 2019_11_25_150520) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
   add_foreign_key "recommendations", "movies"
   add_foreign_key "recommendations", "users"
 end

0c8e4c3f984433fff4f2b87dade2fff769a78b12
Jonathan Pein <jonathan.o.k.pein@gmail.com>
2019-12-05 13:37:32 +0000

    new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,10 @@ Follow.destroy_all
 Recommendation.destroy_all
 Movie.destroy_all
 puts 'destroying users'
-User.destroy_all
-user1 = User.create!(email: "test123@test.com", password: "123456", first_name: "Jonny", last_name: "Pein", bio: 'No pain, no gain')
-user2 = User.create!(email: "test1234@test.com", password: "123456", first_name: "Elanor", last_name: "Hebard", bio: 'this is a bio')
-user3 = User.create!(email: "test12345@test.com", password: "123456", first_name: "Kelly", last_name: "Jeffery", bio: 'this is an other bio')
+# User.destroy_all
+# user1 = User.create!(email: "test123@test.com", password: "123456", first_name: "Jonny", last_name: "Pein", bio: 'No pain, no gain')
+# user2 = User.create!(email: "test1234@test.com", password: "123456", first_name: "Elanor", last_name: "Hebard", bio: 'this is a bio')
+# user3 = User.create!(email: "test12345@test.com", password: "123456", first_name: "Kelly", last_name: "Jeffery", bio: 'this is an other bio')
 
 def fetch_movie_urls
   top_url = "http://www.imdb.com/chart/top"
@@ -71,47 +71,40 @@ urls = [
   'https://images.unsplash.com/photo-1470441623172-c47235e287ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2852&q=80',
   'https://images.unsplash.com/photo-1470406852800-b97e5d92e2aa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80',
 ]
-puts 'creating users'
-count = 0
-10.times do |i|
-  u = User.create!({
-    first_name: Faker::Name.first_name ,
-    last_name: Faker::Name.last_name ,
-    email: Faker::Internet.email,
-    default_image: urls[count],
-    password: "123456",
-    bio: Faker::Quote.matz,
-  })
-  5.times {
-    Recommendation.create!(user: u, movie: Movie.all.sample)
-  }
+# puts 'creating users'
+# count = 0
+# 10.times do |i|
+#   u = User.create!({
+#     first_name: Faker::Name.first_name ,
+#     last_name: Faker::Name.last_name ,
+#     email: Faker::Internet.email,
+#     default_image: urls[count],
+#     password: "123456",
+#     bio: Faker::Quote.matz,
+#   })
+#   5.times {
+#     Recommendation.create!(user: u, movie: Movie.all.sample)
+#   }
 
-  5.times{
-    Bookmark.create!(user: u, movie: Movie.all.sample)
-  }
-  count += 1
-end
+#   5.times{
+#     Bookmark.create!(user: u, movie: Movie.all.sample)
+#   }
+#   count += 1
+# end
 
-users = User.all
-puts 'creating followers'
-users.each do |user|
+# users = User.all
+# puts 'creating followers'
+# users.each do |user|
 
-  20.times do
-    sample = users.sample
-    unless sample == user
-      user.follow(users.sample)
-    end
-  end
-end
+#   20.times do
+#     sample = users.sample
+#     unless sample == user
+#       user.follow(users.sample)
+#     end
+#   end
+# end
 
 # images = ["https://unsplash.com/photos/O0DoKImZQfU", "photo2.jpeg","photo3.jpeg","photo4.jpeg","photo5.jpeg","photo6.jpeg","photo7.jpeg","photo8.jpeg","photo9.jpeg","photo10.jpeg"]
 # 50.times do
 #   image = URI.open(images.sample)
 # end
-
-
-
-
-
-
-
